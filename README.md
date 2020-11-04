a712430b8059d1a853a8158aa42887e55d3ae59e
vighnesh153 <vighnesh.raut13@gmail.com>
2020-11-04 09:44:48 +0530

    disqus plugin and social share buttons

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Vighnesh Raut's Blog
 
 Visit at [blog.vighnesh153.com](https://blog.vighnesh153.com/)
+
+
+### Disqus Admin Board
+https://blog-vighnesh153-com.disqus.com/admin/
--- src/templates/single-post.js
+++ src/templates/single-post.js
@@ -4,12 +4,33 @@ import GatsbyImage from "gatsby-image";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
+import Icons, { IconType } from "../components/icons";
+
+import { DiscussionEmbed } from 'disqus-react';
 
 import constants from "../constants";
+import { slugify } from "../utils";
+
+const SocialLink = ({url, title}) => (
+  <li>
+    <a href={url} target="_blank" rel="noreferrer noopener nofollow">{title}</a>
+  </li>
+);
 
 const SinglePost = ({ data }) => {
-  const { html } = data["markdownRemark"];
+  const { html, id } = data["markdownRemark"];
   const post = data["markdownRemark"].frontmatter;
+
+  const protocol = "https://";
+  const baseURL = "blog.vighnesh153.com/posts/" + slugify(post.title);
+
+  const disqusShortname = "blog-vighnesh153-com";
+  const disqusConfig = {
+    identifier: id,
+    title: post.title,
+    url: protocol + baseURL,
+  };
+
   return (
     <Layout pageHeading={post.title} displayWhiteBackground={true} displayRecentPostsInSidebar={true}>
       <SEO title={post.title} />
@@ -19,6 +40,30 @@ const SinglePost = ({ data }) => {
         </div>
         <div style={styles.date}>{post.date}</div>
         <div style={styles.markdownContent} dangerouslySetInnerHTML={{ __html: html }}/>
+
+        <div>
+          <p style={{ textAlign: 'center' }}>___</p>
+          <br/>
+          <h2 style={{ textAlign: 'center' }}>Share this post</h2>
+          <div>
+            <ul style={styles.socialLinks}>
+              <SocialLink
+                title={<Icons type={IconType.FACEBOOK} width="15px" />}
+                url={"https://facebook.com/sharer/sharer.php?u=" + protocol + baseURL} />
+              <SocialLink
+                title={<Icons type={IconType.TWITTER} />}
+                url={"https://twitter.com/share?url=" + protocol + baseURL + "&text=" + post.title + "&via=blog.vighnesh153.com"} />
+              <SocialLink
+                title={<Icons type={IconType.LINKEDIN} />}
+                url={"https://www.linkedin.com/sharing/share-offsite/?url=" + encodeURI(baseURL)} />
+            </ul>
+
+            <p style={{ textAlign: 'center' }}>___</p>
+            <br/>
+          </div>
+
+          <DiscussionEmbed shortname={disqusShortname} config={disqusConfig} />
+        </div>
       </div>
     </Layout>
   );
@@ -48,6 +93,10 @@ export const postQuery = graphql`
 export default SinglePost;
 
 const styles = {
+  root: {
+    width: '90%',
+    margin: '20px auto',
+  },
   title: {
     marginBottom: '10px',
   },
@@ -60,10 +109,15 @@ const styles = {
     textAlign: 'left',
   },
   markdownContent: {
+    marginBottom: '20px',
     lineHeight: '20px',
   },
-  root: {
-    width: '90%',
-    margin: '20px auto',
+  socialLinks: {
+    margin: '15px 0',
+    display: 'flex',
+    gap: '30px',
+    alignItems: 'center',
+    justifyContent: 'center',
+    listStyle: 'none',
   },
 };
