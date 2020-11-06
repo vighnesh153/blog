ca92e90021c47ece76d7725d8f3f71ffe2ee8719
vighnesh153 <vighnesh.raut13@gmail.com>
2020-11-06 23:25:52 +0530

    add plugins for gatsby-transformer-remark

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,7 +35,28 @@ module.exports = {
         icon: `src/images/icon.png`, // This path is relative to the root of the site.
       },
     },
-    'gatsby-transformer-remark'
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        // CommonMark mode (default: true)
+        commonmark: true,
+        // Footnotes mode (default: true)
+        footnotes: true,
+        // Pedantic mode (default: true)
+        pedantic: true,
+        // GitHub Flavored Markdown mode (default: true)
+        gfm: true,
+        // Plugins configs
+        plugins: [
+          `gatsby-remark-mathjax`,            // Math support
+          `gatsby-remark-autolink-headers`,   // Links to headers (automatically)
+          `gatsby-remark-prismjs`,            // Colored code in code-block
+          `gatsby-remark-copy-linked-files`,  // Copies files to respective folders
+          `gatsby-remark-images`,             // blur-to-focus effect in images in markdown
+          `gatsby-remark-external-links`,     // adds target="_blank" and rel="..." to links
+        ],
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
